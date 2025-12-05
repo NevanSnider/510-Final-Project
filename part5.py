a9c2831b91f8f16877acb007f31942549ae5aca7
NevanSnider <143729670+NevanSnider@users.noreply.github.com>
2025-12-04 19:29:56 -0600

    yippee

--- part5.py
+++ part5.py
@@ -1,53 +1,90 @@
-def accept(input_string):
-    data_structure = open("DataStructure.txt", "r")
-    states = []
-    inputs = []
-    three_ops = ["+", "-", "x"]
-    normal_nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    funky_ones =["(0", "(1", "(2", "(3", "(4", "(5", "(6", "(7", "(8", "(9"]
-    start_state = ""
-    accept_state = ""
-    output = ""
-    curState = ""
-    valid = True
+states = []
+inputs = []
+three_ops = ["+", "-", "x"]
+normal_nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+funky_ones =["(0", "(1", "(2", "(3", "(4", "(5", "(6", "(7", "(8", "(9"]
+start_state = ""
+accept_state = ""
+output = ""
+curState = ""
+valid = True
 
+def tokenizer(text):
+    tokens = []
+    #text is enumerated to be able to get indexes and it reads individual characters
+    for i, ch in enumerate(text):
+        #checks if its an alphanumeric character
+        if ch.isalpha():
+            #if theres a left parenthesis before the character and the next one is a digit then
+            #it is appended to be treated as a single token
+            if (text[i] == '(' and text[i+1].isdigit()):
+                tokens.append(tokens[i] + tokens[i+1])
+
+            #same thing as above but for if there is a / before the parenthesis 
+            elif (text[i] == '(' and text[i-1] == '/'):
+                tokens.append(tokens[i-1] + tokens[i])
+
+            #otherwise append it as normal
+            else:
+                tokens.append(ch)
+    return tokens
+
+
+def accept(input_string):
+    #opens the file to be read
+    data_structure = open("DataStructure.txt", "r")
+
+    #keeps track of the current line being read
     curLine = 1
 
-    myTokens = tokenize(input_string)
-    for line in text:
-        if (curLine = 1):
+    #tokenizes the string to get the special ones in the alphabet
+    myTokens = tokenizer(input_string)
+
+    #These are the things from our data structure we were required to have in there but I dont
+    #think we actually end up needing to use them for the code
+    for line in data_structure:
+        if (curLine == 1):
             states = line.split(" ")
-        else if (curLine = 2):
+        elif (curLine == 2):
             inputs = line.split(" ")
-        else if (curLine = 3):
-            start_state = line
-        else if (curLine = 4):
-            accept_state = line
-            
-        #index 0 is the state being left from,
-        #index 1 is the symbol being read,
-        #index 2 is the variable being popped,
-        #index 3 is the variable being pushed, 
-        #index 4 is the state the transition goes to
-        else if (curLine >= 5):
+        elif (curLine == 3):
+            start_state = line.strip()
+        elif (curLine == 4):
+            global accept_state
+            accept_state = line.strip()
+
+        #reads all the transitions in the data structure and will run through them
             accepted = transition(line, myTokens, 0)
-            if accepted = 1:
-                print("The input is valid!")
-                break
-            if accepted = 0:
-                continue
         curLine += 1
-    print("The the input is invalid")
+
+    #determines if the input is valid
+    if accepted == 1:
+        print("The the input is invalid")
+    if accepted == 0:
+        print("The input is invalid")
 
 
 def transition(line, myTokens, curToken):
+    global curState
+    global stack
+    #sets the initial state to the first one
     if (curState == ""):
         curState = start_state
+    
+    #if we are in the accept state and theres nothing on the stack we return 1 to indicate the input is valid
     if (curState == accept_state):
-        if (stack[-1] = "LAMBDA"):
+        if (stack[-1] == "LAMBDA"):
             return 1
+    #if the stack becomes empty we want it to be lambda
     if len(stack) == 0:
-    stack = ["LAMBDA"]
+        stack = ["LAMBDA"]
+
+    
+    #index 0 is the state being left from,
+    #index 1 is the symbol being read,
+    #index 2 is the variable being popped,
+    #index 3 is the variable being pushed, 
+    #index 4 is the state the transition goes to
     transition_stuff = line.split(" ")
     prestate = transition_stuff[0]
     symbol = transition_stuff[1]
@@ -55,10 +92,16 @@ def transition(line, myTokens, curToken):
     pushed_var = transition_stuff[3]
     poststate = transition_stuff[4]
 
-
-    if (curState = prestate and curToken == symbol and stack[-1] = popped_var):
-        stack.push(pushed_var)
+    #Compares the input with the given transition and will pop and push the necessary variables
+    if (curState == prestate and myTokens[curToken] == symbol and stack[-1] == popped_var):
+        #pop from the stack
+        if (popped_var != "LAMBDA"):
+            stack.pop()
+        #push to the stack
+        stack.append(pushed_var)
+        #update the current state
         curState = poststate
+        #update the current token
         curToken += 1
     else:
         return 0
@@ -67,17 +110,7 @@ def transition(line, myTokens, curToken):
 
     
 
-def tokenizer(text)
-    tokens = []
-    for i, ch in enumerate(text):
-        if ch.isalpha():
-            if (text[i] == '(' and text[i+1].isdigit()):
-                tokens.append[tokens[i] + tokens[i+1]]
-            else if (text[i] == '(' and text[i-1] == '/'):
-                tokens.append[tokens[i-1] + tokens[i]]
-            else:
-                tokens.append(ch)
-    return tokens
+
 
 def main():
     user_input = input("Enter a valid arithmetic equation:\n")
